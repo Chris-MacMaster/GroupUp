2ea0cc4a84bc4390b7445eef5c978505f4d76174
christopher <christopher.macmaster@gmail.com>
2023-04-18 21:14:05 -0700

    small typo

--- python-backend/app/models/group_categories.py
+++ python-backend/app/models/group_categories.py
@@ -2,10 +2,6 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy.sql import func
 
 
-from .db import db, environment, SCHEMA, add_prefix_for_prod
-from sqlalchemy.sql import func
-
-
 group_categories = db.Table("group_categories",
     db.Column('group_id', db.Integer, db.ForeignKey(add_prefix_for_prod('groups.group_id')),
     db.Column('category_id', db.Integer, db.ForeignKey(add_prefix_for_prod('categories.category_id')))))
