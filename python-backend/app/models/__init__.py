a405bb7006decf8155dd17b4d4b537bf25de0b70
christopher <christopher.macmaster@gmail.com>
2023-04-19 15:39:56 -0700

    bug testing

--- python-backend/app/models/__init__.py
+++ python-backend/app/models/__init__.py
@@ -1,6 +1,5 @@
-from .db import db
+from .db import environment, SCHEMA, db
 from .user import User
-from .db import environment, SCHEMA
 
 from .event import Event
 from .group import Group
@@ -9,4 +8,4 @@ from .interest import Interest
 from .category import Category
 from .theme import Theme
 
-from event_image import EventImage
+from .event_image import EventImage
