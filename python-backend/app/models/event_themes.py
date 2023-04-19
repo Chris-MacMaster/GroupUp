bd2de1e0bf442a20dff470fe3a83ac6bb1751d8a
christopher <christopher.macmaster@gmail.com>
2023-04-19 14:46:14 -0700

    added some dependencies from group project

--- python-backend/app/models/event_themes.py
+++ python-backend/app/models/event_themes.py
@@ -1,7 +1,6 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy.sql import func
 
-
 event_themes = db.Table("event_themes",
     db.Model.metadata,
     db.Column('event_id', db.Integer, db.ForeignKey(add_prefix_for_prod('events.id')),
