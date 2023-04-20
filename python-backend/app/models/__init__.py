2fbe48dd5e81ce14b2a2d5124f7cd921a91ec890
christopher <christopher.macmaster@gmail.com>
2023-04-20 15:32:47 -0700

    seeding debugging

--- python-backend/app/models/__init__.py
+++ python-backend/app/models/__init__.py
@@ -9,3 +9,10 @@ from .category import Category
 from .theme import Theme
 
 from .event_image import EventImage
+
+from .user_interests import user_interests
+from .user_groups import user_groups
+from .user_events import user_events
+
+from .group_categories import group_categories
+from .event_themes import event_themes
--- python-backend/app/models/user_events.py
+++ python-backend/app/models/user_events.py
@@ -2,7 +2,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 from sqlalchemy.sql import func
 
 
-user_groups = db.Table("user_events",
+user_events = db.Table("user_events",
     db.Model.metadata,
     db.Column('event_id', db.Integer, db.ForeignKey(add_prefix_for_prod('events.id'))),
     db.Column('user_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'))))
