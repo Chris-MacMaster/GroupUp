328696e533eacc502560c7c85d41369a08dace0d
christopher <christopher.macmaster@gmail.com>
2023-04-27 23:49:37 -0700

    signup validations

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -3,6 +3,8 @@ import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { signUp } from "../../store/session";
 import "./SignupForm.css";
+import { useEffect } from 'react';
+
 
 function SignupFormModal() {
 	const dispatch = useDispatch();
@@ -13,8 +15,31 @@ function SignupFormModal() {
 	const [errors, setErrors] = useState([]);
 	const { closeModal } = useModal();
 
+
+	const [objErrors, setObjErrors] = useState({})
+
+	const [hasSubmitted, setHasSubmitted] = useState(false);
+	const [showPassword, setShowPassword] = useState(false)
+	
+
+
+	useEffect(() => {
+		let e = {}
+		setObjErrors(e)
+		if (!username) e.username = "Must submit a username"
+		if (!email) e.email = "Must submit an email"
+		if (!emailCheck(email)) e.emailCheck = "Invalid email"
+		if (!password) e.password = "Must submit a password"
+		if (password.length < 6) e.passwordLength = "Password must be at least 6 characters"
+		if (password !== confirmPassword) e.confirmPassword = "Passwords must match"
+	}, [username, email, password, confirmPassword])
+
 	const handleSubmit = async (e) => {
 		e.preventDefault();
+		setHasSubmitted(true)
+		console.log("OBJ ERRORS", objErrors)
+		if (Object.values(objErrors).length) return
+
 		if (password === confirmPassword) {
 			const data = await dispatch(signUp(username, email, password));
 			if (data) {
@@ -28,6 +53,19 @@ function SignupFormModal() {
 			]);
 		}
 	};
+
+
+	const togglePassword = (e) => {
+		e.preventDefault()
+		if (showPassword === false) {
+			setShowPassword(true)
+		} else {
+			setShowPassword(false)
+		}
+	}
+
+
+
 
 	return (
 		<>
@@ -44,36 +82,69 @@ function SignupFormModal() {
 						type="text"
 						value={email}
 						onChange={(e) => setEmail(e.target.value)}
-						required
+						// required
 					/>
 				</label>
+				{hasSubmitted && objErrors.email && (
+					<div className='error'>
+						* {objErrors.email}
+					</div>
+				)}
+				{hasSubmitted && objErrors.emailCheck && (
+					<div className='error'>
+						* {objErrors.emailCheck}
+					</div>
+				)}
 				<label>
 					Username
 					<input
 						type="text"
 						value={username}
 						onChange={(e) => setUsername(e.target.value)}
-						required
+						// required
 					/>
 				</label>
+				{hasSubmitted && objErrors.username && (
+					<div className='error'>
+						* {objErrors.username}
+					</div>
+				)}
 				<label>
 					Password
 					<input
-						type="password"
+						type={showPassword === false ? "password" : "text"}
 						value={password}
 						onChange={(e) => setPassword(e.target.value)}
-						required
+						// required
 					/>
+					<div className="toggle-password-div">
+						<i onClick={togglePassword} className="fas fa-eye" />
+					</div>
 				</label>
+				{hasSubmitted && objErrors.password && (
+					<div className='error'>
+						* {objErrors.password}
+					</div>
+				)}
+				{hasSubmitted && objErrors.passwordLength && (
+					<div className='error'>
+						* {objErrors.passwordLength}
+					</div>
+				)}
 				<label>
 					Confirm Password
 					<input
-						type="password"
+						type={showPassword === false ? "password" : "text"}
 						value={confirmPassword}
 						onChange={(e) => setConfirmPassword(e.target.value)}
-						required
+						// required
 					/>
 				</label>
+				{hasSubmitted && objErrors.confirmPassword && (
+					<div className='error'>
+						* {objErrors.confirmPassword}
+					</div>
+				)}
 				<button type="submit">Sign Up</button>
 			</form>
 		</>
@@ -81,3 +152,13 @@ function SignupFormModal() {
 }
 
 export default SignupFormModal;
+
+
+
+export function emailCheck(str) {
+	const regex = /^\S+@\S+\.\S+$/
+	// console.log(!regex.test(email))
+	if (!regex.test(str)) return false
+	return true
+}
+
